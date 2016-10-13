074c980efa10b428709aa97d4dc908e9e0543af1
treeforever <treeforever7@gmail.com>
2016-10-13 18:31:01 +0000

    added Bcryst for user login and register

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+
+  get '/login' => 'sessions#new'
+ post '/login' => 'sessions#create'
+ get '/logout' => 'sessions#destroy'
+
+ get '/signup' => 'users#new'
+ post '/users' => 'users#create'
+
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
@@ -19,22 +28,7 @@ Rails.application.routes.draw do
   end
 
 
-  GifVault::Application.routes.draw do
 
-    # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-    root to: 'gif#cool'
-
-    # I've created a gif controller so I have a page I can secure later.
-    # This is optional (as is the root to: above).
-    get '/cool' => 'gif#cool'
-    get '/sweet' => 'gif#sweet'
-
-    # These routes will be for signup. The first renders a form in the browse, the second will
-    # receive the form and create a user in our database using the data given to us by the user.
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
-
-end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013163315) do
+ActiveRecord::Schema.define(version: 20161013163751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,8 @@ ActiveRecord::Schema.define(version: 20161013163315) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "firstname"
+    t.string   "lastname"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
