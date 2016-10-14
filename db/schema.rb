2095c83c81ef06cd422ae0d9f299e382cdd344c0
treeforever <treeforever7@gmail.com>
2016-10-14 18:54:31 +0000

    backup before re-seed db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013163751) do
+ActiveRecord::Schema.define(version: 20161014171635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,18 @@ ActiveRecord::Schema.define(version: 20161013163751) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "user_id"
+    t.string   "integer"
+    t.string   "description"
+    t.string   "text"
+    t.string   "rating"
+    t.datetime "created_at",  null: false
+    t.string   "datetime"
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
