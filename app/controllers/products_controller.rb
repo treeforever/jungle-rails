7d3dc2b294ce69a979d61d4584f8f5b1e1a98a3d
treeforever <treeforever7@gmail.com>
2016-10-15 14:49:51 +0000

    moved average_rating function to product helper, cleaned the code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-
+  helper ProductHelper
   def index
     @products = Product.all.order(created_at: :desc)
   end
--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,3 +1,5 @@
 module ProductHelper
-  
+  def average_rating(product)
+    pluralize (product.review.sum(:rating) / product.review.length).round(1), 'star'
+  end
 end
